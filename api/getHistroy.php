bf5988609b94791481fffa4e967976fd08b2abbb
Zhuyingyan <zdsfg14@sina.com>
2015-05-04 14:43:12 +0800

    lastest version

---
+++ api/getHistroy.php
@@ -0,0 +1,7 @@
+{"code":0,"msg":"success","data":[
+  {"id":"1","name":"David Wine","desc":"is very good \uff0cI love it yes\uff01\r\nvery very good","pics":["http://img0.bdstatic.com/img/image/shouye/mnxz-9431881658.jpg","http://img0.bdstatic.com/img/image/shouye/mnxz-9431881658.jpg"]}
+  ,
+  {"id":"2","name":"David Wine","desc":"hhaa","pics":["pics/his-lis1.jpg","pics/his-lis2.jpg","pics/his-lis3.jpg"]},
+  {"id":"3","name":"David Wine","desc":"rfgh","pics":["pics/his-lis4.jpg","pics/his-lis5.jpg","pics/his-lis6.jpg"]},
+  {"id":"4","name":"David Wine","desc":"hhgdwer","pics":["pics/his-lis5.jpg","pics/his-lis2.jpg","pics/his-lis4.jpg"]}
+  ]}
